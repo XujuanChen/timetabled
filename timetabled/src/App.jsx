3b5a9590c43fcfbe0e2c96ba544ab14f5a01f782
Xujuan Chen <xchen022@citymail.cuny.edu>
2023-02-24 09:00:08 -0500

    step 3 create a table

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1>Itinerary for 7 Days in New York</h1>
-      <h2>Welcome to New York, Grinner! Check out this calender to get to know the city and see all the sights during your stay.</h2>
+      <h2>Welcome to New York! </h2>
+      <h3>Check out this calender to get to know the city and see all the sights during your stay.</h3>
       <Calendar />
     </div>
   )
